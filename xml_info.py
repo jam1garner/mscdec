4922f77845ddf817fbaef14c84091672cfa5495c
jam1garner <jam1.mcleod@hotmail.com>
2018-11-19 01:32:19 -0500

    Add MscXmlInfo parsing

--- xml_info.py
+++ xml_info.py
@@ -1,3 +1,4 @@
+from xml.etree import ElementTree as ET
 from enum import Enum
 
 class VariableLabel:
@@ -11,5 +12,36 @@ class VariableLabel:
         self.id = id
         self.name = name
         self.methods = []
+
+class MscXmlInfo:
+    def __init__(self, filename=None):
+        self.globals = []
+        self.functions = []
+        self.syscalls = []
+        if file != None:
+              self.read(file)
+
+    def read(filename):
+        labels = ET.parse(filename).getroot()
+        for function in labels.find("functions").findall("function"):
+            self.functions.append(VariableLabel(
+                    function.find("id").text,
+                    function.find("name").text
+                ))
+        for globalNode in labels.find("globals").findall("global"):
+            self.globals.append(VariableLabel(
+                    globalNode.find("id").text,
+                    globalNode.find("name").text
+                ))
+        for syscall in labels.find("syscalls").findall("syscall"):
+            syscallLabel = VariableLabel(
+                        syscall.find("id").text,
+                        syscall.find("name").text
+                    )
+            for method in syscall.find("methods").findall("method"):
+                syscallLabel.methods.append(VariableLabel(
+                        method.find("id").text,
+                        method.find("name").text
+                    ))
 
 
